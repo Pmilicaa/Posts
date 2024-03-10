65749ccd2fc4ba9b3d06915cba9108a071fe767c
milica <micy_98@live.com>
2024-03-10 14:00:33 +0100

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { PostsPage } from "./pages/PostsPage";
+import { ErrorPage } from "./pages/ErrorPage";
+import { PostPage } from "./pages/PostPage";
+import App from "./App";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/pages",
+        element: <PostsPage />,
+      },
+      {
+        path: "pages/:id",
+        element: <PostPage />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
