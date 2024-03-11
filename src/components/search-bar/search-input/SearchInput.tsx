c82a0805e724d6ce4a65cc5bcec810754aaefd70
milica <micy_98@live.com>
2024-03-11 22:53:11 +0100

    fixed searching throw filtered list

--- src/components/search-bar/search-input/SearchInput.tsx
+++ src/components/search-bar/search-input/SearchInput.tsx
@@ -4,13 +4,16 @@ import { usePostStore } from "../../../store/posts-store";
 export const SearchInput = (): ReactElement => {
   const setFilteredPosts = usePostStore((state) => state.setFilteredPosts);
   const currentPagedPosts = usePostStore((state) => state.currentPagedPosts);
+  const filteredPosts = usePostStore((state) => state.filteredPosts);
 
   const setSearchQuery = (value: string): void => {
-    const posts = currentPagedPosts.filter((post) => {
-      return post.title.includes(value);
-    });
+    const posts = value
+      ? filteredPosts.filter((post) => post.title.includes(value))
+      : currentPagedPosts;
+
     setFilteredPosts(posts);
   };
+
   return (
     <input
       name="search"
